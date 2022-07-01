67b5f969e8cbc4f4393b7c23d209aaf10fa75644
Joao Moreira <jgmoreira1809@gmail.com>
2022-07-01 12:04:48 -0700

    Requisito 1 - Adding routes to pages components

---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        404
+      </div>);
+  }
+}
+
+export default NotFound;
