3267db1ff7ba135f42f7d051ba25d9e68afde626
Joao Moreira <jgmoreira1809@gmail.com>
2022-07-05 06:00:02 -0700

    Requisito 7 - adding MusicCard component and rendering songs to the album/:id

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,14 +1,80 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import getMusics from '../services/musicsAPI';
 import Header from './Header';
+import Loading from './Loading';
+import MusicCard from './MusicCard';
 
 class Album extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artistName: '',
+      albumName: '',
+      isLoading: false,
+      songsList: [],
+    };
+  }
+
+  componentDidMount() {
+    this.songs();
+  }
+
+  songs = async () => {
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+    const { songsList } = this.state;
+
+    this.setState({
+      isLoading: true,
+    });
+
+    const music = await getMusics(id);
+
+    this.setState({
+      artistName: music[0].artistName,
+      albumName: music[0].collectionName,
+      isLoading: false,
+      songsList: music,
+    });
+  };
+
   render() {
+    const { artistName, albumName, isLoading, songsList } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        Album
+        { isLoading
+          ? (<Loading />)
+          : (
+            <div>
+              <h2 data-testid="artist-name">{artistName}</h2>
+              <h3 data-testid="album-name">{albumName}</h3>
+              {
+                songsList.filter((element) => element.kind === 'song')
+                  .map((element, index) => (
+                    <div key={ index }>
+                      <MusicCard
+                        trackName={ element.trackName }
+                        previewUrl={ element.previewUrl }
+                      />
+                    </div>
+                  ))
+              }
+            </div>
+          )}
       </div>);
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+}.isRequired;
 
 export default Album;
---
+++ src/pages/MusicCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MusicCard extends React.Component {
+  render() {
+    const { trackName, previewUrl } = this.props;
+
+    return (
+      <>
+        <h4>{ trackName }</h4>
+        <audio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          {' '}
+          <code>audio</code>
+          .
+        </audio>
+      </>
+
+    );
+  }
+}
+
+MusicCard.propTypes = {
+  previewUrl: PropTypes.string.isRequired,
+  trackName: PropTypes.string.isRequired,
+};
+
+export default MusicCard;
