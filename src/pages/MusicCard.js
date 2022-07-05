8b10d603609521141f52f6e9d92cd898fd885127
Joao Moreira <jgmoreira1809@gmail.com>
2022-07-05 13:53:25 -0700

    Requisito 8 com poblema no teste

--- src/pages/MusicCard.js
+++ src/pages/MusicCard.js
@@ -1,21 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import { addSong } from '../services/favoriteSongsAPI';
+import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 class MusicCard extends React.Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     isChecked: false,
-  //   };
-  // }
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+      isChecked: false,
+      favorite: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getFavSongs();
+  }
+
+  getFavSongs = async () => {
+    this.setState({
+      isLoading: true,
+    });
+    const fav = await getFavoriteSongs();
+    console.log(fav);
+    this.setState({
+      isLoading: false,
+      favorite: fav,
+    });
+  }
+
+  handleCheck = async ({ target }) => {
+    const { trackId } = this.props;
+    this.setState({
+      isLoading: true,
+      isChecked: target.checked,
+    });
+    await addSong(trackId);
+
+    this.setState({
+      isLoading: false,
+    });
+  };
 
   render() {
+    const {
+      isLoading,
+      isChecked,
+    } = this.state;
+
     const { trackName,
       previewUrl,
-      // trackId
+      trackId,
     } = this.props;
-
+    console.log(isChecked);
     return (
       <>
         <h4>{ trackName }</h4>
@@ -26,16 +63,19 @@ class MusicCard extends React.Component {
           <code>audio</code>
           .
         </audio>
-        {/* <label data-testid={ `checkbox-music-${trackId}` } htmlFor="favorita">
-          Favorita
-          <input
-            id="favorita"
-            name="favorita"
-            type="checkbox"
-            onChange={ handleCheck }
-            checked={ isChecked }
-          />
-        </label> */}
+        { isLoading
+          ? (<Loading />) : (
+            <label data-testid={ `checkbox-music-${trackId}` } htmlFor="favorita">
+              Favorita
+              <input
+                id="favorita"
+                name="favorita"
+                type="checkbox"
+                onChange={ this.handleCheck }
+                checked={ isChecked }
+              />
+            </label>
+          )}
       </>
 
     );
@@ -45,7 +85,7 @@ class MusicCard extends React.Component {
 MusicCard.propTypes = {
   previewUrl: PropTypes.string.isRequired,
   trackName: PropTypes.string.isRequired,
-  // trackId: PropTypes.string.isRequired,
+  trackId: PropTypes.number.isRequired,
 };
 
 export default MusicCard;
